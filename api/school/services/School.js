56872ae069d040922dd20526fac57c3f554fb05b
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-10-13 21:10:12 -0600

    add additional info array

--- api/school/services/School.js
+++ api/school/services/School.js
@@ -14,7 +14,7 @@ module.exports = {
         model: 'Career',
         populate: {
           path: 'category',
-          model: 'Categories'
+          model: 'Categories',
         }
       });
   }
